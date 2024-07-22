0620c213f33629f79f202d51e71c0cbc9195d2b9
Nitinsegall <nitinsehgal384@gmail.com>
2024-07-22 22:34:52 +0530

    Some changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,43 +4,43 @@ import './App.css'
 
 function App() {
   const [weatherData, setWeatherData] = useState([]);
-  const [searchValue, setSearchValue] = useState("");
+  const [searchValue, setSearchValue] = useState("yamunanagar");
 
-  let url=searchValue
-  if(url ==''){
-    url='yamunanagar'
-  }else{
-    url=searchValue
-  }
+  // let url=searchValue
+  // if(url ==''){
+  //   url='yamunanagar'
+  // }else{
+  //   url=searchValue
+  // }
   
   const weatherResult = async () => {
     try{
-
       const result = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${url}&appid=9f522aa60779df90b025c7db7c3d3862`
+        `https://api.openweathermap.org/data/2.5/weather?q=${searchValue||'yamunanagar'}&appid=9f522aa60779df90b025c7db7c3d3862`
       )
       const data = await result.json();
-      console.log(data);
+      console.log(data,'data');
       setWeatherData({
-        temp: data.main.temp,
-        country: data.sys.country,
-      name: data.name,
-      description: data.weather[0].description,
-      humidity: data.main.humidity,
-      speed: data.wind.speed,
-      sunrise:data.sys.sunrise,
-      sunset:data.sys.sunset,
-      main:data.weather[0].main,
-      message:data.message
+        temp: data?.main?.temp,
+        country: data?.sys?.country,
+      name: data?.name,
+      description: data?.weather?.[0]?.description,
+      humidity: data?.main?.humidity,
+      speed: data?.wind?.speed,
+      sunrise:data?.sys?.sunrise,
+      sunset:data?.sys?.sunset,
+      main:data?.weather?.[0]?.main,
+      message:data?.message
     });
   }catch(err){
     console.log(err);
 }
 
+console.log(weatherData,'weather');
   };
   useEffect(() => {
     weatherResult();
-  }, [url]);
+  }, [searchValue]);
   // console.log(weatherData, "weather Data");
   const degree = parseInt(weatherData.temp) - 273.5;
   const speed = parseInt(weatherData.speed) * 3.6;
